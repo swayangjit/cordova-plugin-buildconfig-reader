7d701b4cb236856f730af573f5e0ba6084d20ca1
swayangjit <swayangjit029@gmail.com>
2019-04-26 10:34:17 +0530

    Issue #SB-11999 feat: Updtaed uri for opening the  playstore .

--- src/android/org/sunbird/config/BuildConfigReaderPlugin.java
+++ src/android/org/sunbird/config/BuildConfigReaderPlugin.java
@@ -92,7 +92,7 @@ public class BuildConfigReaderPlugin extends CordovaPlugin {
     private static void openGooglePlay(CordovaInterface cordova, String appId) {
         try {
             Context context = cordova.getActivity().getApplicationContext();
-            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + appId));
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + appId));
             intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             context.startActivity(intent);
         } catch (ActivityNotFoundException ex) {
